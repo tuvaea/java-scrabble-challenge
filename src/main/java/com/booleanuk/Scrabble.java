494e48bf6babf0d4a84b0263c91bc2e5f51f9594
Tuva Aarseth <tuva.emilie@hotmail.com>
2024-08-13 18:07:27 +0200

    Some clean up

--- src/main/java/com/booleanuk/Scrabble.java
+++ src/main/java/com/booleanuk/Scrabble.java
@@ -7,7 +7,7 @@ public class Scrabble {
     HashMap<Character, Integer> alphabet = new HashMap<>();
     String word;
     public Scrabble(String word) {
-        this.word = word;
+        this.word = word.toUpperCase();
 
         alphabet.put('A', 1);
         alphabet.put('B', 3);
@@ -35,9 +35,6 @@ public class Scrabble {
         alphabet.put('X', 8);
         alphabet.put('Y', 4);
         alphabet.put('Z', 10);
-
-        // Stack<Character> chars = new Stack<>();
-
     }
 
     public int score() {
@@ -47,28 +44,35 @@ public class Scrabble {
         int wordlength = word.length();
 
         for (int i=0; i<wordlength; i++){
-            char c = word.toUpperCase().charAt(i);
+            char c = word.charAt(i);
 
+            // Checks if character is whitespace
             if (Character.isWhitespace(c)){
                 i++;
 
+            // If double word or double letter
             } else if (i==0 & c=='{') {
+                // If double letter
                 if (word.charAt(2) == '}'){
-                    totalscore += (alphabet.get(word.toUpperCase().charAt(i+1)) * 2);
+                    totalscore += (alphabet.get(word.charAt(i+1)) * 2);
                     i++;
                 }
+                // If double word
                 else if (word.charAt(wordlength-1) == '}'){
                     doubleword = 2;
+                    // If (double word and) triple letter or triple word
                     if (word.charAt(1) == '['){
+                        // If triple letter
                         if (word.charAt(3) == ']'){
-                            totalscore += (alphabet.get(word.toUpperCase().charAt(i+2)) * 3);
-                            i++;
-                            i++;
+                            totalscore += (alphabet.get(word.charAt(i+2)) * 3);
+                            i+=2;
                         }
+                        // If triple word
                         else if (word.charAt(wordlength-2) == ']'){
                             tripleword = 3;
                             i++;
                         }
+                        // If invalid
                         else {
                             return 0;
                         }
@@ -79,23 +83,29 @@ public class Scrabble {
                 }
             }
 
+            // If triple word or triple letter
             else if (i==0 & c=='[') {
+                // If triple letter
                 if (word.charAt(2) == ']'){
-                    totalscore += (alphabet.get(word.toUpperCase().charAt(i+1)) * 3);
+                    totalscore += (alphabet.get(word.charAt(i+1)) * 3);
                     i++;
                 }
+                // If triple word
                 else if (word.charAt(wordlength-1) == ']'){
                     tripleword = 3;
+                    // If (triple word and) double word or double letter
                     if (word.charAt(1) == '{'){
+                        // If double letter
                         if (word.charAt(3) == '}'){
-                            totalscore += (alphabet.get(word.toUpperCase().charAt(i+2)) * 2);
-                            i++;
-                            i++;
+                            totalscore += (alphabet.get(word.charAt(i+2)) * 2);
+                            i+=2;
                         }
+                        // If double word
                         else if (word.charAt(wordlength-2) == '}'){
                             doubleword = 2;
                             i++;
                         }
+                        // If invalid
                         else {
                             return 0;
                         }
@@ -104,52 +114,54 @@ public class Scrabble {
                 else {
                     return 0;
                 }
-
+            // Check for validity
             } else if (c=='}') {
                 if (i>1 && word.charAt(i-2) == '{'){
-                    totalscore += 0;
+
                 } else if (word.charAt(0)=='{') {
-                    totalscore += 0;
+
                 } else if (word.charAt(1)=='{') {
-                    totalscore += 0;
+
                 } else{
                     return 0;
                 }
-
+            // Check for validity
             } else if (c==']') {
                 if (i>1 && word.charAt(i-2) == '['){
-                    totalscore += 0;
+
                 } else if (word.charAt(0)=='[') {
-                    totalscore += 0;
+
                 } else if (word.charAt(1)=='[') {
-                    totalscore += 0;
+
                 } else{
                     return 0;
                 }
+            // If double letter
             } else if (i!=0 & c == '{') {
                 if (word.charAt(i+2) == '}'){
-                    totalscore += (alphabet.get(word.toUpperCase().charAt(i+1)) * 2);
+                    totalscore += (alphabet.get(word.charAt(i+1)) * 2);
                     i++;
                 }
                 else {
                     return 0;
                 }
+            // If triple letter
             } else if (i!=0 & c == '[') {
                 if (word.charAt(i+2) == ']'){
-                    totalscore += (alphabet.get(word.toUpperCase().charAt(i+1)) * 3);
+                    totalscore += (alphabet.get(word.charAt(i+1)) * 3);
                     i++;
                 }
                 else {
                     return 0;
                 }
+            // If normal letter
             } else if (Character.isLetter(c)) {
                 totalscore += alphabet.get(c);
             } else {
                     return 0;
                 }
             }
-
+        // Calculate total
         return totalscore * doubleword * tripleword;
     }
-
 }
